c88488fe723c825e822f7c8534340226ea022c8c
murodrigue <musonirodriguez@gmail.com>
2025-02-21 11:19:18 +0200

    Add files via upload

---
+++ exa4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 80, b = 20;
+cout << "Sum: " << a + b << endl;
+cout << "Difference: " << a - b << endl;
+cout << "Product: " << a * b << endl;
+cout << "Quotient: " << a / b << endl;
+cout << "Remainder: " << a % b << endl;
+
+    return 0;
+}
+
---
+++ examp5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string favoriteColor;
+	cout << "Enter your favorite color: ";
+	cin >> favoriteColor;
+	cout << "Wow! " << favoriteColor <<" is a great color!" <<endl;
+return 0;
+}
